fc458b289dd5caa4e57060648238cd98ea311165
yeongwave <playinlion@likelion.org>
2018-11-09 23:05:44 +0900

    Portal
    div id=root 밖의 요소도 터치터치 가능

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { createPortal } from 'react-dom';
+
+class Portals extends Component {
+  render() {
+    return createPortal(<Message />, document.getElementById('touchme'));
+  }
+}
+
+const Message = () => "Now I Can touch this!!";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return(
+      <Fragment>
+        <div className="App" />
+        <Portals />
+      </Fragment>
+    ) 
+    
   }
 }
 
